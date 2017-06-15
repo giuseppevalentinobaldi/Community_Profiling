f0f495163be2fe5262744aa88987fa919b14a195
giuseppe <giuseppe@localhost>
2017-06-15 19:05:25 +0200

    change helloworld

--- Neo4j/src/main/java/helloworld/HelloWorldExample.java
+++ Neo4j/src/main/java/helloworld/HelloWorldExample.java
@@ -1,7 +1,5 @@
-package helloword;
+package helloworld;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.List;
 
 import org.neo4j.driver.v1.AuthTokens;
@@ -15,10 +13,8 @@ import org.neo4j.driver.v1.Values;
 
 import twitter4j.Status;
 import twitter4j.Twitter;
-import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
 
 public class HelloWorldExample implements AutoCloseable {
 	private final Driver driver;
