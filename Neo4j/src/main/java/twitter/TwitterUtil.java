4d7b53850a43c3558594229d8de564bfc5a10791
giuseppe <giuseppe@localhost>
2017-06-16 21:33:23 +0200

    list data item

--- Neo4j/src/main/java/twitter/TwitterUtil.java
+++ Neo4j/src/main/java/twitter/TwitterUtil.java
@@ -1,11 +1,15 @@
 package twitter;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import twitter4j.HashtagEntity;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.URLEntity;
+import twitter4j.UserMentionEntity;
 import twitter4j.auth.AccessToken;
 
 public class TwitterUtil {
@@ -13,24 +17,104 @@ public class TwitterUtil {
 	final private String tokenSecret = "TWieXfhALOSL2meTuxzdKo9gYtnY6viEGeeASKwwV1aUc";
 	final private String consumerKey = "N2LZiDdNAqY1qtgJ8EPRoAdx9";
 	final private String consumerSecret = "ayLGG7YtnVykMbkfNZ3XyYZRo1FDCC4sIO8VBSJELBOoM6lYHU";
-	
+
 	private Twitter twitter;
-	
+
 	public TwitterUtil() {
 		TwitterFactory factory = new TwitterFactory();
 		AccessToken accessToken = new AccessToken(this.getToken(), this.getTokenSecret());
 		this.twitter = factory.getInstance();
-		this.twitter.setOAuthConsumer(this.getConsumerKey(),this.getConsumerSecret());
+		this.twitter.setOAuthConsumer(this.getConsumerKey(), this.getConsumerSecret());
 		this.twitter.setOAuthAccessToken(accessToken);
 	}
-	
-	public void Tweets() throws TwitterException{
+
+	public void Tweets() throws TwitterException {
 		List<Status> statuses = this.twitter.getUserTimeline(769181646176284672L);
-	    System.out.println("Showing home timeline.");
-	    for (Status status : statuses) {
-	        System.out.println(status.getUser().getName() + ":" +
-	                           status.getText());
-	    }
+		System.out.println("Showing home timeline.");
+		for (Status status : statuses) {
+			System.out.println(status.getUser().getName() + ":" + status.getText());
+		}
+	}
+
+	public List<List<String>> getTweetDataItems(long userId) throws TwitterException {
+		List<Status> statuses = this.twitter.getUserTimeline(userId);
+		List<List<String>> listOfDataItem = new LinkedList<List<String>>();
+		for (Status status : statuses) {
+			listOfDataItem.add(newItem(status));
+		}
+		return listOfDataItem;
+	}
+
+	public List<String> newItem(Status status){
+		String temp;
+		boolean isFirst;
+		List<String>  item= new LinkedList<String>();
+		item.add(status.getText());
+		long[] contributors =status.getContributors();
+		temp="";
+		isFirst=true;
+		for (long idContributor : contributors) {
+			if(isFirst){
+				temp+=idContributor;
+				isFirst=false;
+			}
+			else
+				temp+="\t"+idContributor;
+		}
+		item.add(temp);
+		item.add(status.getGeoLocation().getLongitude()+"");
+		item.add(status.getGeoLocation().getLatitude()+"");
+		item.add(status.getCreatedAt()+"");
+		item.add(status.getCreatedAt()+"");
+		HashtagEntity[] hashtagEntities = status.getHashtagEntities();
+		temp="";
+		isFirst=true;
+		for (HashtagEntity hashtagEntity : hashtagEntities) {
+			if(isFirst){
+				temp+=hashtagEntity.getText();
+				isFirst=false;
+			}
+			else
+				temp+="\t"+hashtagEntity.getText();
+		}
+		item.add(temp);
+		URLEntity[] URLEntities = status.getURLEntities();
+		temp="";
+		isFirst=true;
+		for (URLEntity URLEntity : URLEntities) {
+			if(isFirst){
+				temp+=URLEntity.getText();
+				isFirst=false;
+			}
+			else
+				temp+="\t"+URLEntity.getText();
+		}
+		item.add(temp);
+		UserMentionEntity[] userMentionEntities = status.getUserMentionEntities();
+		temp="";
+		isFirst=true;
+		for (UserMentionEntity userMentionEntity : userMentionEntities) {
+			if(isFirst){
+				temp+=userMentionEntity.getText();
+				isFirst=false;
+			}
+			else
+				temp+="\t"+userMentionEntity.getText();
+		}
+		item.add(temp);
+		item.add(status.getFavoriteCount()+"");
+		item.add(status.getId()+"");
+		item.add(status.getInReplyToScreenName());
+		item.add(status.getInReplyToStatusId()+"");
+		item.add(status.getInReplyToUserId()+"");
+		item.add(status.getLang());
+		item.add(status.getRetweetCount()+"");
+		item.add(status.getSource());
+		item.add(status.getUser().getName());
+		item.add(status.getGeoLocation().toString());
+		item.add(status.getUser().getScreenName());
+		item.add(status.getUser().getId()+"");
+		return item;
 	}
 
 	public Twitter getTwitter() {
@@ -40,7 +124,7 @@ public class TwitterUtil {
 	public void setTwitter(Twitter twitter) {
 		this.twitter = twitter;
 	}
-	
+
 	public String getToken() {
 		return token;
 	}
