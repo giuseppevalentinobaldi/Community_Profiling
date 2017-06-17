171641cdf62f02178f346e48ce05287f06e1342a
giuseppe <giuseppe@localhost>
2017-06-17 18:47:11 +0200

    commit

--- Neo4j/src/main/java/neo4j/Neo4jUtil.java
+++ Neo4j/src/main/java/neo4j/Neo4jUtil.java
@@ -9,30 +9,35 @@ import org.neo4j.driver.v1.Values;
 import twitter.TwitterUserData;
 
 public class Neo4jUtil {
-	
+
 	private final Driver driver;
 	private final Session session;
-	
-	public Neo4jUtil(String uri, String user, String password){
-		
+
+	public Neo4jUtil(String uri, String user, String password) {
+
 		this.driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
 		this.session = this.driver.session();
-		
+
 	}
-	
-	public void close(){
-		
+
+	public void close() {
+
 		this.session.close();
 		this.driver.close();
-		
+
 	}
-	
-	public void printUserData (TwitterUserData twitterUserData){
-		session.run("CREATE (a:TwitterUser {user_id: {user_id}, name: {name}, screen_name: {screen_name}})", Values.parameters("user_id", twitterUserData.getId(), "name", twitterUserData.getName(), "screen_name", twitterUserData.getScreenName() ));
-		//session.run("CREATE (a:Person {name: {name}, last_name: {last_name}})", Values.parameters("name", "Maria", "last_name", "Verdi"));
-		
-		//creazione archo
-		//.session.run("MATCH (a:Person),(b:Person) WHERE a.name = 'Mario' AND b.name = 'Maria' CREATE (a)-[r:RELTYPE]->(b)");		
-	}	
+
+	public void printUserData(TwitterUserData twitterUserData) {
+		session.run("CREATE (a:TwitterUser {user_id: {user_id}, name: {name}, screen_name: {screen_name}})",
+				Values.parameters("user_id", twitterUserData.getId(), "name", twitterUserData.getName(), "screen_name",
+						twitterUserData.getScreenName()));
+		// session.run("CREATE (a:Person {name: {name}, last_name:
+		// {last_name}})", Values.parameters("name", "Maria", "last_name",
+		// "Verdi"));
+
+		// creazione archo
+		// .session.run("MATCH (a:Person),(b:Person) WHERE a.name = 'Mario' AND
+		// b.name = 'Maria' CREATE (a)-[r:RELTYPE]->(b)");
+	}
 
 }
