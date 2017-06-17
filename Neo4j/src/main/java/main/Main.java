04dc58be5711fd824cbd32a240bcb48d70b30f45
giuseppe <giuseppe@localhost>
2017-06-17 17:28:51 +0200

    main update

--- Neo4j/src/main/java/main/Main.java
+++ Neo4j/src/main/java/main/Main.java
@@ -1,18 +1,16 @@
 package main;
 
-import java.util.List;
-
 import neo4j.Neo4jUtil;
+import twitter.TwitterUserData;
 import twitter.TwitterUtil;
 
-public class Main{
+public class Main {
 
 	public static void main(String args[]) throws Exception {
 		TwitterUtil twitter = new TwitterUtil();
-		List<List<String>> lls=twitter.getTweetDataItems(769181646176284672L);
+		TwitterUserData twitterUserData = twitter.getUserData(769181646176284672L);
 		@SuppressWarnings("resource")
-		Neo4jUtil neo4j= new Neo4jUtil("bolt://localhost:7687", "neo4j", "neo4j");
-		lls.forEach( l1 -> neo4j.printTweets(l1));
+		Neo4jUtil neo4j = new Neo4jUtil("bolt://localhost:7687", "neo4j", "neo4j");
 		System.exit(0);
 	}
 }
