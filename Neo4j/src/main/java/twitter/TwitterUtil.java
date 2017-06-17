170b4825bbc07e69905f2bb0cba8d650d043774c
marco <marco@marco-pc>
2017-06-17 15:59:43 +0200

    TwitterUtil extends TwitterOntologyUser

--- Neo4j/src/main/java/twitter/TwitterUtil.java
+++ Neo4j/src/main/java/twitter/TwitterUtil.java
@@ -1,19 +1,16 @@
 package twitter;
 
-//import java.util.LinkedList;
 import java.util.List;
 
-//import twitter4j.GeoLocation;
-//import twitter4j.HashtagEntity;
 import twitter4j.Status;
 import twitter4j.Twitter;
-//import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-//import twitter4j.URLEntity;
-//import twitter4j.UserMentionEntity;
 import twitter4j.auth.AccessToken;
 
-public class TwitterUtil {
+import ontology.TwitterOntologyUser;
+
+public class TwitterUtil extends TwitterOntologyUser {
+	
 	final private String token = "769181646176284672-0IC2vOHqXZ22Rxe6inpBCYAecQsZouN";
 	final private String tokenSecret = "TWieXfhALOSL2meTuxzdKo9gYtnY6viEGeeASKwwV1aUc";
 	final private String consumerKey = "N2LZiDdNAqY1qtgJ8EPRoAdx9";
