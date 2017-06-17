250b3caea545fcd803660efe717aa61180752f8b
giuseppe <giuseppe@localhost>
2017-06-17 16:12:19 +0200

    ontology update

---
+++ Neo4j/src/main/java/ontology/TwitterOntologyData.java
@@ -0,0 +1,194 @@
+package ontology;
+
+public class TwitterOntologyData {
+
+	private final String idLabel = "";
+	private final String idDescription = "";
+
+	private final String messageLabel = "";
+	private final String messageDescription = "";
+
+	private final String contributorsIdLabel = "";
+	private final String contributorsIdDescription = "";
+
+	private final String longitudeLabel = "";
+	private final String longitudeDescription = "";
+
+	private final String latitudeLabel = "";
+	private final String latitudeDescription = "";
+
+	private final String timeStampLabel = "";
+	private final String timeStampDescription = "";
+
+	private final String hashTagLabel = "";
+	private final String hashTagDescription = "";
+
+	private final String urlLabel = "";
+	private final String urlDescription = "";
+
+	private final String mentionedUserIdLabel = "";
+	private final String mentionedUserIdDescription = "";
+
+	private final String countFavoriteUserLabel = "";
+	private final String countFavoriteUserDescription = "";
+
+	private final String screenNameRespondedUserLabel = "";
+	private final String screenNameRespondedUserDescription = "";
+
+	private final String tweetIdRespondedLabel = "";
+	private final String tweetIdRespondedDescription = "";
+
+	private final String userIdRespondedLabel = "";
+	private final String userIdRespondedDescription = "";
+
+	private final String languageLabel = "";
+	private final String languageDescription = "";
+
+	private final String countRetweetLabel = "";
+	private final String countRetweetDescription = "";
+
+	private final String sourceLabel = "";
+	private final String sourceDescription = "";
+
+	private final String coordinatesLabel = "";
+	private final String coordinatesDescription = "";
+	
+	//data ontology 
+
+	public String getIdLabel() {
+		return idLabel;
+	}
+
+	public String getIdDescription() {
+		return idDescription;
+	}
+
+	public String getMessageLabel() {
+		return messageLabel;
+	}
+
+	public String getMessageDescription() {
+		return messageDescription;
+	}
+
+	public String getContributorsIdLabel() {
+		return contributorsIdLabel;
+	}
+
+	public String getContributorsIdDescription() {
+		return contributorsIdDescription;
+	}
+
+	public String getLongitudeLabel() {
+		return longitudeLabel;
+	}
+
+	public String getLongitudeDescription() {
+		return longitudeDescription;
+	}
+
+	public String getLatitudeLabel() {
+		return latitudeLabel;
+	}
+
+	public String getLatitudeDescription() {
+		return latitudeDescription;
+	}
+
+	public String getTimeStampLabel() {
+		return timeStampLabel;
+	}
+
+	public String getTimeStampDescription() {
+		return timeStampDescription;
+	}
+
+	public String getHashTagLabel() {
+		return hashTagLabel;
+	}
+
+	public String getHashTagDescription() {
+		return hashTagDescription;
+	}
+
+	public String getUrlLabel() {
+		return urlLabel;
+	}
+
+	public String getUrlDescription() {
+		return urlDescription;
+	}
+
+	public String getMentionedUserIdLabel() {
+		return mentionedUserIdLabel;
+	}
+
+	public String getMentionedUserIdDescription() {
+		return mentionedUserIdDescription;
+	}
+
+	public String getCountFavoriteUserLabel() {
+		return countFavoriteUserLabel;
+	}
+
+	public String getCountFavoriteUserDescription() {
+		return countFavoriteUserDescription;
+	}
+
+	public String getScreenNameRespondedUserLabel() {
+		return screenNameRespondedUserLabel;
+	}
+
+	public String getScreenNameRespondedUserDescription() {
+		return screenNameRespondedUserDescription;
+	}
+
+	public String getTweetIdRespondedLabel() {
+		return tweetIdRespondedLabel;
+	}
+
+	public String getTweetIdRespondedDescription() {
+		return tweetIdRespondedDescription;
+	}
+
+	public String getUserIdRespondedLabel() {
+		return userIdRespondedLabel;
+	}
+
+	public String getUserIdRespondedDescription() {
+		return userIdRespondedDescription;
+	}
+
+	public String getLanguageLabel() {
+		return languageLabel;
+	}
+
+	public String getLanguageDescription() {
+		return languageDescription;
+	}
+
+	public String getCountRetweetLabel() {
+		return countRetweetLabel;
+	}
+
+	public String getCountRetweetDescription() {
+		return countRetweetDescription;
+	}
+
+	public String getSourceLabel() {
+		return sourceLabel;
+	}
+
+	public String getSourceDescription() {
+		return sourceDescription;
+	}
+
+	public String getCoordinatesLabel() {
+		return coordinatesLabel;
+	}
+
+	public String getCoordinatesDescription() {
+		return coordinatesDescription;
+	}
+
+}
--- Neo4j/src/main/java/ontology/TwitterOntologyUser.java
+++ Neo4j/src/main/java/ontology/TwitterOntologyUser.java
@@ -2,30 +2,37 @@ package ontology;
 
 public class TwitterOntologyUser {
 	
-	private final String nameLabel="tweet user";
-	private final String nameDescription="A data item about the user account which generated the associated tweet data item.";
-	
+	private final String nameLabel = "tweet user";
+	private final String nameDescription = "A data item about the user account which generated the associated tweet data item.";
+
 	private final String screenNameLabel = "twitter user screen name";
 	private final String screenNameDescricption = "";
 
 	private final String idLabel = "twitter user id";
 	private final String idDescricption = "";
 	
+	//user data ontology 
+	
 	public String getNameLabel() {
 		return nameLabel;
 	}
+
 	public String getNameDescription() {
 		return nameDescription;
 	}
+
 	public String getScreenNameLabel() {
 		return screenNameLabel;
 	}
+
 	public String getScreenNameDescricption() {
 		return screenNameDescricption;
 	}
+
 	public String getIdLabel() {
 		return idLabel;
 	}
+
 	public String getIdDescricption() {
 		return idDescricption;
 	}
