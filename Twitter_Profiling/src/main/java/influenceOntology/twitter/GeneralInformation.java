f2d71afa00410c728942045b2f7c387d0edfa63a
giuseppe <giuseppe@localhost>
2017-06-20 23:36:40 +0200

    app modeling 011

--- Twitter_Profiling/src/main/java/influenceOntology/twitter/GeneralInformation.java
+++ Twitter_Profiling/src/main/java/influenceOntology/twitter/GeneralInformation.java
@@ -1,39 +1,56 @@
 package influenceOntology.twitter;
 
 public class GeneralInformation {
-	private String description; //Provides the description of the account, as set by its owner.
-	private int followers; //Provides the number of the followers of the account.
-	private String displayName; //Provides the name displayed at the web page of the account, as set by its owner.
-	private int following; //Provides the number of the accounts that the account follows.
-	
-	public GeneralInformation(String description, int followers, String displayName, int following){
-		this.description=description;
-		this.followers=followers;
-		this.displayName=displayName;
-		this.following=following;
+	private String description; // Provides the description of the account, as
+								// set by its owner.
+	private int followers; // Provides the number of the followers of the
+							// account.
+	private String displayName; // Provides the name displayed at the web page
+								// of the account, as set by its owner.
+	private int following; // Provides the number of the accounts that the
+							// account follows.
+	//tweets per day
+	//active account
+	//profile locked
+	//retrieved on
+	//retweet percentage
+	//number of tweets
+
+	public GeneralInformation(String description, int followers, String displayName, int following) {
+		this.description = description;
+		this.followers = followers;
+		this.displayName = displayName;
+		this.following = following;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getFollowers() {
 		return followers;
 	}
+
 	public void setFollowers(int followers) {
 		this.followers = followers;
 	}
+
 	public String getDisplayName() {
 		return displayName;
 	}
+
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
 	}
+
 	public int getFollowing() {
 		return following;
 	}
+
 	public void setFollowing(int following) {
 		this.following = following;
 	}
--- Twitter_Profiling/src/main/java/influenceOntology/twitter/QualityMetrics.java
+++ Twitter_Profiling/src/main/java/influenceOntology/twitter/QualityMetrics.java
@@ -1,5 +1,31 @@
 package influenceOntology.twitter;
 
 public class QualityMetrics {
+	private float replyRatio; // Provides the ratio of the user's latest tweets
+								// which are used as replies to other users'
+								// tweets.
+	private float influenceMetric; // Indicates the value of the Influence
+									// Metric measurement. Its aim is to
+									// describe both the importance and impact
+									// of an account in a social network.
+	// h-index retweet daily
+	// h-index retweet
+	// h-index favorite daily
+	// h-index favorite
 
+	public float getReplyRatio() {
+		return replyRatio;
+	}
+
+	public void setReplyRatio(float replyRatio) {
+		this.replyRatio = replyRatio;
+	}
+
+	public float getInfluenceMetric() {
+		return influenceMetric;
+	}
+
+	public void setInfluenceMetric(float influenceMetric) {
+		this.influenceMetric = influenceMetric;
+	}
 }
