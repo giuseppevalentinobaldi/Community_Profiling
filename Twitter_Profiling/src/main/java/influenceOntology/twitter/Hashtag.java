15996be3d02a476359d674104abc95e3f04c6e38
giuseppe <giuseppe@localhost>
2017-06-20 23:27:07 +0200

    app modeling 010

--- Twitter_Profiling/src/main/java/influenceOntology/twitter/Hashtag.java
+++ Twitter_Profiling/src/main/java/influenceOntology/twitter/Hashtag.java
@@ -1,5 +1,25 @@
 package influenceOntology.twitter;
 
 public class Hashtag {
+	private String hashtag;
+	
+	public Hashtag(String hashtag){
+		this.setHashtag(hashtag);
+	}
 
+	public String getHashtag() {
+		return hashtag;
+	}
+
+	public void setHashtag(String hashtag) {
+		this.hashtag = hashtag;
+	}
+	
+	public boolean equals(String hashtag){
+		return this.hashtag.equals(hashtag);
+	}
+	
+	public String toString(){
+		return "#"+this.getHashtag();
+	}
 }
