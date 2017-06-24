cf8099dc863c69c831a9229290254b404a5359ad
giuseppe <giuseppe@localhost>
2017-06-24 22:47:42 +0200

    similarity 70%

--- Twitter_Profiling/src/main/java/influenceOntology/twitter/Similarity.java
+++ Twitter_Profiling/src/main/java/influenceOntology/twitter/Similarity.java
@@ -1,5 +1,31 @@
 package influenceOntology.twitter;
 
 public class Similarity {
+	private EntitySimilarity URL;
+	private EntitySimilarity mentions;
+	private EntitySimilarity hashtag;
+	
+	public Similarity(TwitterUserAccount a,TwitterUserAccount b){
+		
+	}
+	
+	public EntitySimilarity getURL() {
+		return URL;
+	}
+	public void setURL(EntitySimilarity uRL) {
+		URL = uRL;
+	}
+	public EntitySimilarity getMentions() {
+		return mentions;
+	}
+	public void setMentions(EntitySimilarity mentions) {
+		this.mentions = mentions;
+	}
+	public EntitySimilarity getHashtag() {
+		return hashtag;
+	}
+	public void setHashtag(EntitySimilarity hashtag) {
+		this.hashtag = hashtag;
+	}
 
 }
